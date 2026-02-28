031274f7b50d9a60745ca891242656ffdaab2722
AHyralExistenceist <junser0317@gmail.com>
2026-02-28 16:43:38 +0900

    Fix base path for GitHub Pages deployment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/harry/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/harrypottercmu/' : '/',
   server: {
     port: 5173,
     proxy: {
